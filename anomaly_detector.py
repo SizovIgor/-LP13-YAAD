0fca477d4e0a4fc3cc91d80144ceab783d3e9dfa
Igor Sizov <31280079+SizovIgor@users.noreply.github.com>
2019-07-13 11:20:00 +0300

    push

--- anomaly_detector.py
+++ anomaly_detector.py
@@ -8,42 +8,41 @@ from sklearn.linear_model import LinearRegression
 from datetime import timedelta, datetime
 
 
-class AnomalyDetector(object):
+class AnomalyDetector:
     def __init__(self):
-        self.standard_deviation = 0
+        self.std = 0
 
-    def fit(self, trained_model):
+    def fit(self, ts, trained_model):
+        y_test = ts['target']
         y_pred = trained_model.t_model.predict(
-            trained_model.prepared['X_test']
+            ts.drop(labels=['target'], axis=1)
         )
-        calaulate_scuare_deviation = pd.DataFrame(
-            index=trained_model.prepared['X_test'].index,
-        )
-        calaulate_scuare_deviation['y_pred'] = y_pred
-        calaulate_scuare_deviation['y_test'] = trained_model.prepared['y_test']
-        calaulate_scuare_deviation['mean'] = calaulate_scuare_deviation.apply(
+        calculate_square_deviation = pd.DataFrame(index=y_test.index)
+        calculate_square_deviation['y_pred'] = y_pred
+        calculate_square_deviation['y_test'] = y_test
+        calculate_square_deviation['mean'] = calculate_square_deviation.apply(
             lambda x: x.mean(),
             axis=1,
         )
-        calaulate_scuare_deviation[
-            'deviation'
-        ] = calaulate_scuare_deviation.apply(
+        calculate_square_deviation[
+            'deviation'] = calculate_square_deviation.apply(
             lambda x: x['y_pred'] - x['mean'],
-            axis=1
+            axis=1,
         )
-        calaulate_scuare_deviation['square_deviation'] = calaulate_scuare_deviation.apply(
+        calculate_square_deviation[
+            'square_deviation'] = calculate_square_deviation.apply(
             lambda x: x['deviation'] ** 2,
-            axis=1
+            axis=1,
         )
-        dispersion = calaulate_scuare_deviation['square_deviation'].sum() / len(
-            calaulate_scuare_deviation['square_deviation'],
+        dispersion = calculate_square_deviation['square_deviation'].sum() / len(
+            calculate_square_deviation['square_deviation'],
         )
-        self.standard_deviation = np.sqrt(dispersion)
-        return self.standard_deviation
+        self.std = np.sqrt(dispersion)
+        return self.std
 
     def detect(self, predict, real_val):
         confidence_intervals = range(
-            predict - self.standard_deviation * 3,
-            predict + self.standard_deviation * 3,
+            predict - self.std * 3,
+            predict + self.std * 3,
         )
         return real_val in confidence_intervals
--- time_series_regressor.py
+++ time_series_regressor.py
@@ -1,11 +1,10 @@
+from datetime import timedelta, datetime
 import pandas as pd
-# import numpy as np
+import numpy as np
 from matplotlib import pyplot as plt
-# from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.linear_model import LinearRegression
-from datetime import timedelta, datetime
 from anomaly_detector import AnomalyDetector
 
 
@@ -20,23 +19,29 @@ def read_json(file='daily-min-temperature.csv', sep=','):
     )
 
 
-class TimeSeriesRegressor(object):
+class TimeSeriesRegressor:
 
-    def __init__(self, ts, granularity=24, num_lags=60, train_test_size=0.33):
-        self.ts = ts
+    def __init__(self, granularity=24, num_lags=60, train_ratio=0.33):
+        self.score = None
         self.granularity = timedelta(hours=granularity)
         self.num_lags = num_lags
-        self.train_test_size = train_test_size
+        self.train_ratio = train_ratio
         self.is_trained = False
         self.t_model = None
 
-    def _make_lags_matrix(self):
-        lags_matrix = pd.DataFrame(index=self.ts[self.num_lags:].index)
-        target = self.ts[self.num_lags:]
-        lags_matrix['target'] = target
-        for num_lag in range(1, self.num_lags + 1):
-            lag_name = f'lag_{num_lag}'
-            lags_matrix[lag_name] = self.ts.shift(num_lag)[self.num_lags:]
+    def _make_lags_matrix(self, ts):
+        values = np.array([
+            ts[row:row + self.num_lags][::-1]
+            for row in range(len(ts) - self.num_lags)
+        ])
+        columns = ['lag_%s' % (i + 1) for i in range(self.num_lags)]
+        lags_matrix = pd.DataFrame(
+            columns=columns,
+            data=values,
+            index=ts[self.num_lags:].index
+        )
+        lags_matrix['target'] = ts[self.num_lags:]
+
         return lags_matrix
 
     @staticmethod
@@ -64,46 +69,41 @@ class TimeSeriesRegressor(object):
         lags_matrix['day'] = lags_matrix.index.map(lambda x: x.day)
         return lags_matrix
 
-    def train(self, train_type: int = 0):
-        if train_type == 1:
-            self._train_gradient_boosting()
-        else:
-            self._train_linear_regression()
+    def train(self, ts, train_type: ["Boosting", "Linear"] = "linear"):
 
-    def _train_linear_regression(self):
-        prepared_param = self._prepare()
+        feature = self._feature_enrichment(self._make_lags_matrix(ts))
+
+        X = feature.drop(labels=['target'], axis=1)
+        y = feature['target']
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y,
+            test_size=self.train_ratio
+        )
+
+        if train_type.lower() == "boosting":
+            self._train_gradient_boosting(X_train, y_train)
+        elif train_type.lower() == "linear":
+            self._train_linear_regression(X_train, y_train)
+
+        self.score = self.t_model.score(X_test, y_test)
+
+        X['target'] = y
+        X.to_csv('all_ts.csv')
+
+        X_test['target'] = y_test
+        X_test.to_csv('test_split.csv')
+
+    def _train_linear_regression(self, X_train, y_train):
+        # prepared_param = self._prepare()
         self.t_model = LinearRegression(n_jobs=-1)
-        self.t_model.fit(prepared_param['X_train'], prepared_param['y_train'])
+        self.t_model.fit(X_train, y_train)
         self.is_trained = True
-        # y_pred = self.t_model.predict(prepared_param['X_test'])
-        # print(r2_score(prepared_param['y_test'], y_pred))
-        # prepared_param['y_test'].plot(figsize=(20, 20))
-        # pd.Series(y_pred, index=prepared_param['y_test'].index).plot()
-        # feature_importances = {
-        #     feature: feature_value
-        #     for feature, feature_value
-        #     in zip(prepared_param['X'].columns, self.t_model.coef_)
-        # }
-        # print(sorted(feature_importances.items(), key=lambda x: x[1]))
 
-    def _train_gradient_boosting(self):
-        prepared_param = self._prepare()
+    def _train_gradient_boosting(self, X_train, y_train):
+        # prepared_param = self._prepare()
         self.t_model = GradientBoostingRegressor()
-        self.t_model.fit(prepared_param['X_train'], prepared_param['y_train'])
+        self.t_model.fit(X_train, y_train)
         self.is_trained = True
-        # self.plotting = pd.Series(
-        #     y_pred,
-        #     index=prepared_param['y_test'].index,
-        # )
-        # plt.show()
-        # feature_importances = {
-        #     feature: feature_value
-        #     for feature, feature_value in zip(
-        #     prepared_param['X'].columns,
-        #     self.t_model.feature_importances_,
-        # )
-        # }
-        # print(sorted(feature_importances.items(), key=lambda x: x[1]))
 
     def _generate_next_row(self, ts):
         one_raw_data = ts[:-self.num_lags - 1:-1].values.reshape(
@@ -120,30 +120,31 @@ class TimeSeriesRegressor(object):
         )
         return self._feature_enrichment(next_row), next_time
 
-    def _parse_timestamp(self, requested_date):
-        current_date = self.ts.index[-1]
+    def _parse_timestamp(self, ts, requested_date):
+        current_date = ts.index[-1]
         number_of_time_interval = int(
             (requested_date - current_date) / self.granularity,
         )
         if number_of_time_interval > 0:
             return number_of_time_interval
 
-    def _is_entry_to_ts(self, requested_date):
-        return requested_date in self.ts
+    @staticmethod
+    def _is_entry_to_ts(ts, requested_date):
+        return requested_date in ts
 
-    def predict(self, n_points=1, iso_timestamp=''):
+    def predict_history(self, ts, n_points=1, iso_timestamp=''):
         if not self.is_trained:
             return
         if iso_timestamp:
             requested_date = datetime.fromisoformat(iso_timestamp)
-            if self._is_entry_to_ts(requested_date):
-                return self.ts[self.ts.index == requested_date]
+            if self._is_entry_to_ts(ts, requested_date):
+                return ts[ts.index == requested_date]
 
-            n_points = self._parse_timestamp(requested_date)
+            n_points = self._parse_timestamp(ts, requested_date)
             if not n_points:
                 return None
 
-        local_ts = self.ts
+        local_ts = ts
         predicted_ts = pd.Series()
         for _ in range(n_points + 1):
             lags_matrix, next_time = self._generate_next_row(local_ts)
@@ -165,44 +166,22 @@ class TimeSeriesRegressor(object):
         # plt.show()
         return predicted_ts
 
-    def _prepare(self):
-        self.prepared = {}
-        self.lags_matrix = self._make_lags_matrix()
-        self.prepared['feature_matrix'] = self._feature_enrichment(
-            lags_matrix=self.lags_matrix,
-        )
-        self.prepared['X'] = self.prepared['feature_matrix'].drop(
-            labels=['target'],
-            axis=1,
-        )
-        self.prepared['y'] = self.prepared['feature_matrix']['target']
-        train_dictionary = [
-            'X_train',
-            'X_test',
-            'y_train',
-            'y_test',
-        ]
-        train_tuple = train_test_split(
-            self.prepared['X'],
-            self.prepared['y'],
-            test_size=self.train_test_size,
-        )
-        self.prepared.update(zip(train_dictionary, train_tuple))
-        return self.prepared  # ToDO: изменить название, оно не понятно
-
 
 if __name__ == '__main__':
-    trained_model = TimeSeriesRegressor(read_json(), num_lags=60)
-    trained_model.train(0)
+    trained_model = TimeSeriesRegressor(num_lags=60)
+    trained_model.train(read_json(), 'Linear')
     print(f'The model was train: {trained_model.is_trained}')
     print(
         'This is predict: ',
-        trained_model.predict(n_points=10),
+        trained_model.predict_history(read_json(), n_points=10),
     )
-    a_detector = AnomalyDetector().fit(trained_model)
-
+    a_detector = AnomalyDetector().fit(read_json("test_split.csv"),
+                                       trained_model)
+    print(f'Standard deviation (test_split): {a_detector}')
+    a_detector = AnomalyDetector().fit(read_json("all_ts.csv"),
+                                       trained_model)
     # finish
-    print(f'Standard deviation: {a_detector}')
+    print(f'Standard deviation (all_ts): {a_detector}')
     print('The end')
 
 # %load_ext autoreload
